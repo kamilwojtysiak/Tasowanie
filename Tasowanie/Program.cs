ae44dd28383b7e24967a68fdd8ef0ffaf2ab4450
kamilwojtysiak <kamil@futrzane.pl>
2020-06-23 13:56:45 +0200

    Tasowanie

---
+++ Tasowanie/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Tasowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> values = new List<string>();
+
+            using (var reader = new StreamReader(@"C:\Projekty\Tasowanie\dane.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    values.Add(line);
+                }
+            }
+
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+
+            listA.Add(values[0]);
+            listB.Add(values[1]);
+
+        }
+    }
+}
